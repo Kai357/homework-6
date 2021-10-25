df493b2300f0dd190710284a3c8984a04a2ab027
Kai <mordechaigerstley@gmail.com>
2021-10-25 12:19:14 -0400

    made api work

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const requestUrl =
+  "https://api.openweathermap.org/data/2.5/weather?q=london&appid=ed05974d9df9d894a078b4e44ff8c225";
+function getApi() {
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+}
 
+console.log(getApi());
