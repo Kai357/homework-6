035891cc4b3e2f81853e6d6a8ec5842fa25129ee
Kai <mordechaigerstley@gmail.com>
2021-10-25 22:22:58 -0400

    added a comment

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const requestUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q=london&appid=ed05974d9df9d894a078b4e44ff8c225";
+const requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=london&appid=ed05974d9df9d894a078b4e44ff8c225`;
 function getApi() {
   fetch(requestUrl)
     .then(function (response) {
@@ -9,5 +8,4 @@ function getApi() {
       console.log(data);
     });
 }
-
-console.log(getApi());
+console.log(getApi.main);
