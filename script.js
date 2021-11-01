e112c6c097353b30316f1b7cc584d3a0fde10e2c
Kai <mordechaigerstley@gmail.com>
2021-11-01 15:05:05 -0400

    made javascript work

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-const requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=london&appid=ed05974d9df9d894a078b4e44ff8c225`;
-function getApi() {
+const buttonEle = document.querySelector("form");
+const input = document.getElementById("user-input");
+let locationName = document.getElementById("local");
+const temp = document.getElementById("temp");
+var img = document.getElementById("icon");
+var message = document.getElementById("temp-discription");
+function getApi(city) {
+  let requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ed05974d9df9d894a078b4e44ff8c225&units=imperial`;
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       console.log(data);
+      locationName.innerHTML = data.name;
+      temp.innerHTML = data.main.temp;
+      var icon = data.weather[0].icon;
+      console.log(icon);
+      img.src = "http://openweathermap.org/img/w/" + icon + ".png";
     });
 }
-console.log(getApi.main);
+buttonEle.addEventListener("submit", (event) => {
+  event.preventDefault();
+  getApi(input.value);
+});
+//console.log(getApi());
